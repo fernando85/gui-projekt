bca04c57d170155f62bc72141ed3b0eedf89346e
Fernando Luwinda <fernando_conan@yahoo.com>
2012-06-21 20:27:46 +0200

    Exceptionsrumpf geschrieben.

---
+++ Graphen/src/exception/NotEulerschException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class NotEulerschException extends Exception {
+
+	public NotEulerschException() {
+		super("NotEulerschException");
+	}
+}
---
+++ Graphen/src/exception/UnConnectedException.java
@@ -0,0 +1,9 @@
+package exception;
+
+public class UnConnectedException extends Exception {
+
+	public UnConnectedException() {
+		super("UnConnectedException");
+	}
+
+}
